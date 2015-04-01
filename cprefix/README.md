a763e96650a6bb88653ec594018af52762743df3
Emmanuel Odeke <odeke@ualberta.ca>
2015-04-01 05:42:00 +0000

    common prefix util added

---
+++ cprefix/README.md
@@ -0,0 +1,16 @@
+## cprefix
+
+Find the common prefix between sequences
+
+# Installation:
+
+```shell
+$ go get github.com/odeke-em/xon/cprefix
+```
+
+# Usage:
+
+```shell
+$ cprefix /mnt/flux/grox /mnt/sync/a1/a3/ /mnt/slides/9kA/2015
+/mnt/
+```
---
+++ cprefix/commonPrefix.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "fmt"
+    "os"
+)
+
+func commonPrefix(values ...string) string {
+	vLen := len(values)
+	if vLen < 1 {
+		return ""
+	}
+	minIndex := 0
+	min := values[0]
+	minLen := len(min)
+
+	for i := 1; i < vLen; i += 1 {
+		st := values[i]
+		if st == "" {
+			return ""
+		}
+		lst := len(st)
+		if lst < minLen {
+			min = st
+			minLen = lst
+			minIndex = i + 0
+		}
+	}
+
+	prefix := make([]byte, minLen)
+	matchOn := true
+	for i := 0; i < minLen; i += 1 {
+		for j, other := range values {
+			if minIndex == j {
+				continue
+			}
+			if other[i] != min[i] {
+				matchOn = false
+				break
+			}
+		}
+		if !matchOn {
+			break
+		}
+		prefix[i] = min[i]
+	}
+	return string(prefix)
+}
+
+func main() {
+    rest := os.Args[1:]
+    cprefix := commonPrefix(rest...)
+    if cprefix != "" {
+        fmt.Println(cprefix)
+    }
+}
